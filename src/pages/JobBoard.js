42e61a9f18fa002d500e709cefbcd30bf5177991
Maria Davis <mdavis81@my.bcit.ca>
2020-04-29 10:03:01 -0700

    Added job board page

---
+++ src/pages/JobBoard.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+import { Card, Logo, Form, Input, Button } from '../components/AuthForm';
+
+
+function JobBoard() {
+  return (
+    <Card>
+      <Form>
+        <Button>Search
+        </Button>
+      </Form>
+    </Card>
+  );
+}
+
+export default JobBoard;
