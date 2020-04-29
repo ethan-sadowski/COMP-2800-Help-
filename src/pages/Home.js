51b93ea997f7c2bf4569c993b4ea55923babfaf3
chase-lsc <chase_lsc@hotmail.com>
2020-04-29 12:50:32 -0700

    deleted redundant pages, added go to job board without signing in to Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,15 @@
 
 import React from "react";
+import IndividualHome from "./IndividualHome";
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
 
 function Home(props) {
-  return <div>Home Page</div>;
+  return (
+    <div><p>Home Page</p>
+    <Link to="/jobs">Go to job board without signing in.</Link>
+    <Route path="/jobs" component={IndividualHome} />
+    </div>
+  )
 }
 
 export default Home;
